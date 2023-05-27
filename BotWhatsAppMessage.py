0b5f707bd4cdf03fa3b6c5777fb7864f9a9a4e7a
Malozx <134738911+Malozx@users.noreply.github.com>
2023-05-26 23:05:29 -0300

    Update BotWhatsAppMessage.py

--- BotWhatsAppMessage.py
+++ BotWhatsAppMessage.py
@@ -5,5 +5,5 @@ messageZap = ['Hello', 'Hello World'] # Enter the messages you want to send in r
 num = '+55' # Enter the number you want to send the message.
 
 pywhatkit.sendwhatmsg(num, random.choice(messageZap), 6, random.randint(0, 36))
-# If you want to send specific messages, just remove the "random.choice", I also recommend using remove the vector and using just string.
+# If you want to send specific messages, just remove the "random.choice", I also recommend removing the vector and just using string.
 # With "random.randint" it's basically the same thing, it's the minutes, just remove and insert the minutes of your choice.
